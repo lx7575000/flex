c6ac9f758e14002ff82967f18df2fd0d9ca9dd79
lx7575000 <liu_narcisus@163.com>
2016-01-03 10:20:20 +0800

    添加了gulp-wrap插件 制作了整体框架模板，以后只需要更改具体内容就好了

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,25 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
+const wrap = require('gulp-wrap');
 
-
-
+function handleError(err){
+	console.log(err.toString());
+	this.emit('end');
+}
 
 gulp.task('sass', () => {
 	gulp.src('./styles/main.scss')
-		.pipe(sass())
+		.pipe(sass()).on('error', handleError)
 		.pipe(prefix())
 		.pipe(gulp.dest('../styles'));
 });
 
-gulp.task('cp', ()=>{
-	gulp.src('index.html')
-		.pipe(gulp.dest('../'));
+// gulp.task('cp', ()=>{
+// 	gulp.src('index.html')
+// 		.pipe(gulp.dest('../'));
+// });
+
+gulp.task('build', () => {
+	gulp.src('pages/*.html')
+		.pipe(wrap({src: "layout/default.html"}))
+		.pipe(gulp.dest('../'));	
 });
 
 gulp.task('watch', () => {
-	gulp.watch(['*.html'], ['cp']);
+	gulp.watch(['**/*.html'], ['build']);
 	gulp.watch(['styles/*.scss'], ['sass']);
 })
 
-gulp.task('default', ['sass', 'cp', 'watch']);
+gulp.task('default', ['sass', 'build', 'watch']);
