10b9f2923ebf35238b1cfae5d0717d6fd94ba42e
lx7575000 <liu_narcisus@163.com>
2016-01-03 11:09:23 +0800

    footer添加效果

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -13,10 +13,11 @@ gulp.task('sass', () => {
 	gulp.src('./styles/main.scss')
 		.pipe(sass()).on('error', handleError)
 		.pipe(prefix())
-		.pipe(gulp.dest('../styles'));
+		.pipe(gulp.dest('../styles'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('browser-sync', () => {
+gulp.task('browser-sync', ['sass', 'build', 'cp'], () => {
 	browserSync({
 		server: {
 			baseDir: '..'
@@ -24,10 +25,10 @@ gulp.task('browser-sync', () => {
 	});
 });
 
-// gulp.task('cp', ()=>{
-// 	gulp.src('index.html')
-// 		.pipe(gulp.dest('../'));
-// });
+gulp.task('cp', ()=>{
+	return gulp.src('js/main.js')
+          .pipe(gulp.dest('../js'));
+});
 
 gulp.task('build', () => {
 	gulp.src('pages/*.html')
@@ -35,9 +36,14 @@ gulp.task('build', () => {
 		.pipe(gulp.dest('../'));	
 });
 
+gulp.task('rebuild', ['build'], () =>{
+	browserSync.reload();
+});
+
 gulp.task('watch', () => {
-	gulp.watch(['**/*.html'], ['build']);
+	gulp.watch(['**/*.html'], ['rebuild']);
 	gulp.watch(['styles/*.scss'], ['sass']);
+	gulp.watch(['js/main.js'], ['cp']);
 })
 
-gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
